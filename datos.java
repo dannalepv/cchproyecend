159e348bf9e26be2e79bc07e8915a27d3522fd7a
danna <hapycat13@gmail.com>
2020-04-23 11:21:10 -0500

    Intercambio inicial de proyecto

---
+++ datos.java
@@ -0,0 +1,11 @@
+
+public class datos
+{
+   
+    public static void main (String[]args)
+    {
+    System.out.println("Nombre:Daniela Alexandra Pablo Vega");
+    System.out.print("N°cta:318347033");
+    System.out.print("Fecha:08_01_2020");
+    }    
+}
